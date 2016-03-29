be5b891b58bc81843c0f8b84cb79896a7bca1edc
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-03-28 20:46:03 -0400

    update user profile display

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -12,12 +12,19 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 		get "/"
 		assert_template "posts/index"
 		assert_select "h1", text: @user.name
+		assert_select "h1>img.gravatar"
+		assert_select "p.email", text: @user.email
+		assert_select "p.age", text: "30 years old"
+		assert_select "p.location", text: "From #{@user.location}"
 	end
 
 	test "should show other user profile on user page" do
 		get user_url(@other_user)
 		assert_template "users/show"
 		assert_select "h1", text: @other_user.name
+		assert_select "h1>img.gravatar"
+		assert_select "p.email", text: @other_user.email
+		assert_select "p.location", text: "From #{@other_user.location}"
 	end
 
 end
