c58f8e76f714d11a7a7c9e4c66b29dd1144bcb5d
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-04-11 19:32:15 -0400

    post edits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def time_ago_or_date(date, max_ago=2.days.ago, format="%B %d, %Y")
+		if date < max_ago
+			date.strftime(format)
+		else
+			time_ago_in_words(date) + " ago"
+		end
+	end
+
 end
