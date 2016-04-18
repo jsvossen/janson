ceeb374388ce92a2aa82236e0bc90d6da064ac79
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-04-17 20:22:21 -0400

    limit comment display in news feed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,4 +11,13 @@ class Comment < ActiveRecord::Base
 	scope :order_desc, -> { order(created_at: :desc) }
 	scope :most_recent, -> (limit=5) { order(created_at: :desc).limit(limit) }
 
+	def body_trimmed(limit = 450)
+		if body.size > limit 
+			trimmed = body[0..limit]
+			trimmed = trimmed + "..." if trimmed.size < body.size
+		else
+			body
+		end
+	end
+
 end
