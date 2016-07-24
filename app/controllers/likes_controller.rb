cf0b01b36e2ea2dca5b265759ea7b3a5ee26af91
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-07-24 13:15:53 -0400

    Ajaxify likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,18 +6,35 @@ class LikesController < ApplicationController
 		@like = current_user.likes.build(like_params)
 		if @like.save
 			@like.notifications.create(user: @like.post.user) if @like.user != @like.post.user
-			flash[:success] = "Post liked!"
-			redirect_to_back_or_default
+			
+			respond_to do |format|
+				format.html { 
+					flash[:success] = "Post liked!"
+					redirect_to_back_or_default
+				}
+				format.js
+			end
 		else
-			flash[:danger] = "Error: #{@like.errors.full_messages.join('. ')}."
-			redirect_to_back_or_default
+			respond_to do |format|
+				format.html { 
+					flash[:danger] = "Error: #{@like.errors.full_messages.join('. ')}."
+					redirect_to_back_or_default 
+				}
+				format.js
+			end
 		end
 	end
 
 	def destroy
-		current_user.likes.find_by(id: params[:id]).destroy
-		flash[:success] = "Post unliked."
-		redirect_to_back_or_default
+		@like = current_user.likes.find_by(id: params[:id])
+		@like.destroy
+		respond_to do |format|
+			format.html { 
+				flash[:success] = "Post unliked."
+				redirect_to_back_or_default 
+			}
+			format.js
+		end
 	end
 
 	private
